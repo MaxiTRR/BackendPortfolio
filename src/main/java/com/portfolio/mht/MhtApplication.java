b0607defaf5ed9a98fdc8633a2c518d8823b28a5
MaxiTRR <maxihtorres@gmail.com>
2022-08-10 18:46:07 -0300

    Primer commit del Backend

---
+++ src/main/java/com/portfolio/mht/MhtApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/mht/MhtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
