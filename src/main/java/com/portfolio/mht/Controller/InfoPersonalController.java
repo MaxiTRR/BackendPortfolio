ec7ff755262485c5b352dd752df1cd636f417b41
MaxiTRR <maxihtorres@gmail.com>
2022-08-16 10:55:02 -0300

    Modificacion del Controller (metodo getInfoPersonal por ID)

--- src/main/java/com/portfolio/mht/Controller/InfoPersonalController.java
+++ src/main/java/com/portfolio/mht/Controller/InfoPersonalController.java
@@ -5,6 +5,7 @@ import com.portfolio.mht.Entity.InfoPersonal;
 import com.portfolio.mht.Interface.IInfoPersonalService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class InfoPersonalController {
     @Autowired IInfoPersonalService iInfoPersonalService;
     
@@ -52,5 +54,10 @@ public class InfoPersonalController {
         
         iInfoPersonalService.saveInfoPersonal(infoPersonal);
         return infoPersonal;
+    }
+    
+    @GetMapping("infoPersonal/traer/perfil")
+    public InfoPersonal findInfoPersonal(){
+        return iInfoPersonalService.findInfoPersonal((long)2);
     }
 }
